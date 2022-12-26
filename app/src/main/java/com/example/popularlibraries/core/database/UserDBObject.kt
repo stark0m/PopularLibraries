4fb1eda8932df9974e4fea8918938b8cca1e6457
ilya <011283im@gmail.com>
2022-12-26 17:36:32 +0300

    подключили room

---
+++ app/src/main/java/com/example/popularlibraries/core/database/UserDBObject.kt
@@ -0,0 +1,19 @@
+package com.example.popularlibraries.core.database
+
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "users")
+data class UserDBObject(
+    @PrimaryKey
+    @ColumnInfo(name = PRIMARY_KEY)
+    val id: Long,
+    val login: String,
+    val avatarUrl: String?
+) {
+
+    companion object {
+        const val PRIMARY_KEY = "id"
+    }
+}
