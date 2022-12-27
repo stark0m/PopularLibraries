49138ce4919ca8e50de077685aa40f3feceabe70
ilya <011283im@gmail.com>
2022-12-27 15:29:57 +0300

    подключили Dagger2

---
+++ app/src/main/java/com/example/popularlibraries/di/AppComponent.kt
@@ -0,0 +1,9 @@
+package com.example.popularlibraries.di
+
+import com.mirkhusainov.geekbrainscourse.repository.GithubUserRepository
+import dagger.Component
+
+@Component
+interface AppComponent {
+//    fun getUsersRepo():GithubUserRepository
+}
